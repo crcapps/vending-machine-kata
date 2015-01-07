e4f0e844152303d84820c782aeaff7f2133abfd9
Casey Ryan Capps <crcapps@gmail.com>
2015-01-07 10:26:17 -0500

    Added Vending Machine model

---
+++ ios/VendingMachineKata/VendingMachineKata/Models/VendingMachine.m
@@ -0,0 +1,13 @@
+//
+//  VendingMachine.m
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/7/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import "VendingMachine.h"
+
+@implementation VendingMachine
+
+@end
