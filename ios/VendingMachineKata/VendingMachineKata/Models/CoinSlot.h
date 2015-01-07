f8be44731e4b8b257f82983456907c70b7444719
Casey Ryan Capps <crcapps@gmail.com>
2015-01-07 14:28:36 -0500

    Methods to CoinSlot header

--- ios/VendingMachineKata/VendingMachineKata/Models/CoinSlot.h
+++ ios/VendingMachineKata/VendingMachineKata/Models/CoinSlot.h
@@ -10,4 +10,13 @@
 
 @interface CoinSlot : NSObject
 
+/** The current total value of the inserted coins */
+@property (nonatomic, strong, readonly)   NSDecimalNumber  *currentTotalValue;
+
+/** Represents a coin being dropped into the slot,
+ reading the diameter and mass from hardware.
+ Returns the value of the coin.
+ */
+- (NSDecimalNumber *)dropCoinWithDiameter:(NSNumber *)diameter Mass:(NSNumber *)mass Thickness:(NSNumber *)thickness;
+
 @end
