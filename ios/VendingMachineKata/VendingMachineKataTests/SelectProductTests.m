6425250a4668b6b5446e6db0ae134c2a97016422
Casey Ryan Capps <crcapps@gmail.com>
2015-01-08 14:45:27 -0500

    Added Select Product feature tests

---
+++ ios/VendingMachineKata/VendingMachineKataTests/SelectProductTests.m
@@ -0,0 +1,32 @@
+//
+//  SelectProductTests.m
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/8/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+/*
+ As a vendor
+ I want customers to select products
+ So that I can give them an incentive to put money in the machine
+ */
+
+@interface SelectProductTests : XCTestCase
+
+@end
+
+@implementation SelectProductTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
