086cafecb93df17d3f6fca06340037105e15a33c
Casey Ryan Capps <crcapps@gmail.com>
2015-01-08 09:49:00 -0500

    Made CoinData a class, since struct was a vestige of another way

---
+++ ios/VendingMachineKata/VendingMachineKata/Models/CoinData.h
@@ -0,0 +1,32 @@
+//
+//  CoinData.h
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/7/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/** Enumerates the types of coins recognized (or not) */
+typedef NS_ENUM(NSInteger, CoinType) {
+    kCoinTypeSlug,
+    kCoinTypePenny,
+    kCoinTypeNickel,
+    kCoinTypeDime,
+    kCoinTypeQuarter
+};
+
+/** Represents data about an identified coin */
+@interface CoinData : NSObject
+
+/** The type of coin */
+@property (nonatomic)   CoinType    coinType;
+
+/** The value of the coin */
+@property (nonatomic)   NSDecimal   coinValue;
+
+/** Whether or not this coin is accepted by the machine */
+@property (nonatomic)   BOOL        isAccepted;
+
+@end
---
+++ ios/VendingMachineKata/VendingMachineKata/Models/CoinData.m
@@ -0,0 +1,13 @@
+//
+//  CoinData.m
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/7/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import "CoinData.h"
+
+@implementation CoinData
+
+@end
