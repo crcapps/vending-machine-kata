36c32b0336185e200ac5a1a6afe75c87894f34d6
Casey Ryan Capps <crcapps@gmail.com>
2015-01-15 14:59:00 -0500

    Accept Coins Tests

---
+++ ios/VendingMachineKata/VendingMachineKataTests/AcceptCoinsTests.m
@@ -0,0 +1,59 @@
+//
+//  AcceptCoinsTests.m
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/15/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+/**
+ Accept Coins
+ 
+ As a vendor
+ I want a vending machine that accepts coins
+ So that I can collect money from the customer
+ 
+ The vending machine will accept valid coins (nickels, dimes, and quarters) and reject invalid ones (pennies). When a valid coin is inserted the amount of the coin will be added to the current amount and the display will be updated. When there are no coins inserted, the machine displays INSERT COIN. Rejected coins are placed in the coin return.
+
+ */
+
+@interface AcceptCoinsTests : XCTestCase
+
+@end
+
+@implementation AcceptCoinsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testItAcceptsValidCoins {
+    XCTFail(@"This test is not yet implemented.");
+}
+
+- (void)testItRejectsInvalidCoins {
+    XCTFail(@"This test is not yet implemented.");
+}
+
+- (void)testItAddsValueOfInsertedCoinToCurrentAmountAndUpdatesDisplay {
+    XCTFail(@"This test is not yet implemented.");
+}
+
+- (void)testItDisplaysCorrectMessageWhenNoCoinsAreInserted {
+    XCTFail(@"This test is not yet implemented.");
+}
+
+- (void)testItPlacesRejectedCoinsInTheCoinReturn {
+    XCTFail(@"This test is not yet implemented.");
+}
+
+@end
