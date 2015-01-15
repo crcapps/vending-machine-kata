b6016463620d8d9ee84e740aac7081aaa43a346a
Casey Ryan Capps <crcapps@gmail.com>
2015-01-15 14:32:52 -0500

    Created new Xcode project

---
+++ ios/VendingMachineKata/VendingMachineKata/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VendingMachineKata
+//
+//  Created by Casey Ryan Capps on 1/15/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/VendingMachineKata/VendingMachineKata/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VendingMachineKata
+//
+//  Created by Casey Ryan Capps on 1/15/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
