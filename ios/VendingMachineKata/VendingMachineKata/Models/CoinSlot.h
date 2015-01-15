6c25f85416fb311aaa4291ce0c36cbfdad8c867f
Casey Ryan Capps <crcapps@gmail.com>
2015-01-15 15:07:12 -0500

    CoinSlot model

---
+++ ios/VendingMachineKata/VendingMachineKata/Models/CoinSlot.h
@@ -0,0 +1,13 @@
+//
+//  CoinSlot.h
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/15/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CoinSlot : NSObject
+
+@end
