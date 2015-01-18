e3bbe4d445d8f3aa1afae310012a75f941610c9c
Casey Ryan Capps <crcapps@gmail.com>
2015-01-18 14:11:31 -0500

    Coin Bank model tests

--- ios/VendingMachineKata/VendingMachineKata/Models/CoinBank.h
+++ ios/VendingMachineKata/VendingMachineKata/Models/CoinBank.h
@@ -15,10 +15,7 @@
 /** Represents the collection of coins inserted into the slot */
 @property (nonatomic, strong, readonly) CoinBag *bankedCoins;
 
-/** If the bank contains the magic threshold to make change for anything */
+/** If the bank contains the magic threshold to make change for anything (at least 3 coins totalling at least 15 cents) */
 @property (nonatomic, readonly) BOOL canMakeChangeForAnything;
-
-/** Can the bank make change for this amount? */
-- (BOOL)canMakeChangeForAmount:(NSDecimalNumber *)amount onPrice:(NSDecimalNumber *)price withCoinsInserted:(CoinBag *)coins;
 
 @end
--- ios/VendingMachineKata/VendingMachineKataTests/CoinBankTests.m
+++ ios/VendingMachineKata/VendingMachineKataTests/CoinBankTests.m
@@ -11,6 +11,7 @@
 
 #import "CoinBank.h"
 #import "CoinData.h"
+#import "CoinBag.h"
 #import "NSDecimalNumber+Currency.h"
 
 @interface CoinBankTests : XCTestCase
@@ -24,6 +25,21 @@ CoinBank *coinBank;
 - (void)setUp {
     [super setUp];
     coinBank = [CoinBank new];
+}
+
+- (void)testCanMakeChangeForAnything {
+    CoinBag *bag = [CoinBag new];
+    XCTAssertFalse(coinBank.canMakeChangeForAnything, @"*** Coin bank can somehow make change when empty!");
+    [coinBank.bankedCoins addCoin:[CoinData dime] amount:1];
+    [coinBank.bankedCoins addCoin:[CoinData nickel] amount:2];
+    XCTAssert(coinBank.canMakeChangeForAnything, @"*** Coin bank can't make change when sufficient coinage banked!");
+    [coinBank.bankedCoins emptyInto:bag];
+    [coinBank.bankedCoins addCoin:[CoinData dime] amount:2];
+    [coinBank.bankedCoins addCoin:[CoinData nickel] amount:1];
+    XCTAssert(coinBank.canMakeChangeForAnything, @"*** Coin bank can't make change when sufficient coinage banked!");
+    [coinBank.bankedCoins emptyInto:bag];
+    [coinBank.bankedCoins addCoin:[CoinData nickel] amount:3];
+    XCTAssert(coinBank.canMakeChangeForAnything, @"*** Coin bank can't make change when sufficient coinage banked!");
 }
 
 @end
