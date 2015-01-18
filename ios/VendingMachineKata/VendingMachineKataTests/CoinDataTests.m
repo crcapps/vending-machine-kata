e9673a9a7f223e0c36b4eabb660aca62e3f58c71
Casey Ryan Capps <crcapps@gmail.com>
2015-01-18 10:15:09 -0500

    Might as well use built in object equality testing

--- ios/VendingMachineKata/VendingMachineKataTests/CoinDataTests.m
+++ ios/VendingMachineKata/VendingMachineKataTests/CoinDataTests.m
@@ -30,42 +30,42 @@ CoinData *coinData;
     CoinData *coinData = [CoinData identifyCoinForDiameter:@24.26 mass:@5.670 thickness:@1.75];
     
     XCTAssertEqual(kCoinTypeQuarter, coinData.coinType, "*** Quarter was not recognized! Recognizer returned %@ instead.", coinData.name);
-    XCTAssert([[CoinData quarter] isEqual:coinData], @"*** Coin recognizer returned incorrect object!");
+    XCTAssertEqualObjects([CoinData quarter], coinData, @"*** Coin recognizer returned incorrect object!");
 }
 
 - (void)testItRecognizesDimeAndReturnsCorrectObject {
     CoinData *coinData = [CoinData identifyCoinForDiameter:@17.91 mass:@2.268 thickness:@1.35];
     
     XCTAssertEqual(kCoinTypeDime, coinData.coinType, "*** Dime was not recognized! Recognizer returned %@ instead.", coinData.name);
-    XCTAssert([[CoinData dime] isEqual:coinData], @"*** Coin recognizer returned incorrect object!");
+    XCTAssertEqualObjects([CoinData dime], coinData, @"*** Coin recognizer returned incorrect object!");
 }
 
 - (void)testItRecognizesNickelAndReturnsCorrectObject {
     CoinData *coinData = [CoinData identifyCoinForDiameter:@21.21 mass:@5.000 thickness:@1.95];
     
     XCTAssertEqual(kCoinTypeNickel, coinData.coinType, "*** Nickel was not recognized! Recognizer returned %@ instead.", coinData.name);
-    XCTAssert([[CoinData nickel] isEqual:coinData], @"*** Coin recognizer returned incorrect object!");
+    XCTAssertEqualObjects([CoinData nickel], coinData, @"*** Coin recognizer returned incorrect object!");
 }
 
 - (void)testItRecognizesPennyAndReturnsCorrectObject {
     CoinData *coinData = [CoinData identifyCoinForDiameter:@19.05 mass:@2.500 thickness:@1.52];
     
     XCTAssertEqual(kCoinTypePenny, coinData.coinType, "*** Penny was not recognized! Recognizer returned %@ instead.", coinData.name);
-    XCTAssert([[CoinData penny] isEqual:coinData], @"*** Coin recognizer returned incorrect object!");
+    XCTAssertEqualObjects([CoinData penny], coinData, @"*** Coin recognizer returned incorrect object!");
 }
 
 - (void)testItRecognizesSlugAndReturnsCorrectObject {
     CoinData *coinData = [CoinData identifyCoinForDiameter:@24.26 mass:@5.000 thickness:@1.52];
     
     XCTAssertEqual(kCoinTypeSlug, coinData.coinType, "*** Slug was not recognized! Recognizer returned %@ instead.", coinData.name);
-    XCTAssert([[CoinData slug] isEqual:coinData], @"*** Coin recognizer returned incorrect object!");
+    XCTAssertEqualObjects([CoinData slug], coinData, @"*** Coin recognizer returned incorrect object!");
 }
 
 - (void)testItReturnsSlugTypeForNilInputEdgeCase{
     CoinData *coinData = [CoinData identifyCoinForDiameter:nil mass:nil thickness:nil];
     
     XCTAssertEqual(kCoinTypeSlug, coinData.coinType, "*** Slug was not recognized! Recognizer returned %@ instead.", coinData.name);
-    XCTAssert([[CoinData slug] isEqual:coinData], @"*** Coin recognizer returned incorrect object!");
+    XCTAssertEqualObjects([CoinData slug], coinData, @"*** Coin recognizer returned incorrect object!");
 }
 
 @end
