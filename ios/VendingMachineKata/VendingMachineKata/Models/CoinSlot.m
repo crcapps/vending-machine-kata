73e332dd06df7bcb4b4d51874c09f1265b3cdf57
Casey Ryan Capps <crcapps@gmail.com>
2015-01-17 20:41:30 -0500

    CoinSlot and Display models and tests added

---
+++ ios/VendingMachineKata/VendingMachineKata/Models/CoinSlot.m
@@ -0,0 +1,13 @@
+//
+//  CoinSlot.m
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/17/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import "CoinSlot.h"
+
+@implementation CoinSlot
+
+@end
---
+++ ios/VendingMachineKata/VendingMachineKata/Models/Display.h
@@ -0,0 +1,13 @@
+//
+//  Display.h
+//  VendingMachineKata
+//
+//  Created by the Heatherness on 1/17/15.
+//  Copyright (c) 2015 Casey Ryan Capps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Display : NSObject
+
+@end
